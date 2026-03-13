eea05ab8203d4f04129a222089e5b4b5d94b235c
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-12 23:20:19 -0500

    Configuracion inicial de JwtModule con configutacion de secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -10,7 +13,26 @@ import { User } from './entities/user.entity';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET') || '',
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
+    // JwtModule.register({
+    //   secret: '',
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ],
   exports: [
     TypeOrmModule
