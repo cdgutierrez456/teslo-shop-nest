61a40132e1f7e4196aa05f3e61425cd85e9c6bee
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-12 23:42:35 -0500

    Finalizando implementacion de JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,11 +8,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
@@ -29,7 +31,10 @@ import { User } from './entities/user.entity';
     })
   ],
   exports: [
-    TypeOrmModule
+    TypeOrmModule,
+    JwtStrategy,
+    PassportModule,
+    JwtModule
   ]
 })
 export class AuthModule {}
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,15 +1,40 @@
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
 
 import { User } from "../entities/user.entity";
 import { JwtPayload } from "../interfaces/jwt-payload.interface";
+import { Repository } from "typeorm";
+import { ConfigService } from '@nestjs/config';
+import { UnauthorizedException } from "@nestjs/common";
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    configService: ConfigService
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET') || '',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    })
+  }
 
   async validate(payload: JwtPayload): Promise<User> {
     const { email } = payload
 
-    return ;
+    const user = await this.userRepository.findOneBy({ email })
+
+    if (!user)
+      throw new UnauthorizedException('Token not valid')
+
+    if (!user.isActive)
+      throw new UnauthorizedException('User is inactive, talk with an admin.')
+
+    return user;
   }
 
 }
