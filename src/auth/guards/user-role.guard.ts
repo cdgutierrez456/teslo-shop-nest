31fbb19c95870b0d8fab406d177d347079917fba
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-16 20:25:48 -0500

    Modificando logica en validacion de user-role

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -1,6 +1,8 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -15,7 +17,22 @@ export class UserRoleGuard implements CanActivate {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler())
 
-    console.log(validRoles);
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest()
+    const user = req.user as User;
+
+    if (!user)
+      throw new BadRequestException('User not found')
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) return true;
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role [${validRoles}]`
+    )
 
 
     return true;
