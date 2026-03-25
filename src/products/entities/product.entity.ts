927e195eb817c32da3497ae74e42106ca5303830
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-24 23:43:35 -0500

    Agrendo personalizacion superficial a controlador de productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,58 +7,98 @@ import { User } from "../../auth/entities/user.entity";
 @Entity({ name: 'products' })
 export class Product {
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '94f05e2a-32c4-4942-8c6f-9049d3870c2e',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '94f05e2a-32c4-4942-8c6f-9049d3870c2e',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @Column('text', {
     unique: true
   })
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '94f05e2a-32c4-4942-8c6f-9049d3870c2e',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @Column('float', {
     default: 0
   })
   price: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '94f05e2a-32c4-4942-8c6f-9049d3870c2e',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @Column('text', {
     nullable: true
   })
   description: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '94f05e2a-32c4-4942-8c6f-9049d3870c2e',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @Column('text', {
     unique: true
   })
   slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '20',
+    description: 'Stock Quantity',
+    uniqueItems: true
+  })
   @Column('int', {
     default: 0
   })
   stock: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '["S","M"]',
+    description: 'Product ID',
+    uniqueItems: true
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'men/women',
+    description: 'Gender',
+    uniqueItems: true
+  })
   @Column('text')
   gender: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '["shirt", "jacket"]',
+    description: 'Product tags',
+    uniqueItems: true
+  })
   @Column('text', {
     array: true,
     default: []
   })
   tags: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: '["1740176-00-A_0_2000.jpg","1740176-00-A_1.jpg"]',
+    description: 'Image IDs',
+    uniqueItems: true
+  })
   @OneToMany(
     () => ProductImage,
     productImage => productImage.product,
