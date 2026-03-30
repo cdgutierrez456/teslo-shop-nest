fb48304696ac3df3e5b39992e62470989d599a52
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-30 16:44:41 -0500

    Verificando token de usuario conectado a websocket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,19 +1,31 @@
+import { JwtService } from '@nestjs/jwt';
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 import { MessagesWsService } from './messages-ws.service';
 import { NewMessageDto } from './dtos/new-message.dto';
 
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
+
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
+  ) {}
 
   handleConnection(client: Socket, ...args: any[]) {
     const token = client.handshake.headers.authentication as string;
-    console.log(token);
+    let payload: JwtPayload
+    try {
+      payload = this.jwtService.verify(token)
+    } catch (error) {
+      client.disconnect()
+      return;
+    }
 
     this.messagesWsService.registerClient(client)
     this.wss.emit('clients-updated', this.messagesWsService.getconnectedClients())
