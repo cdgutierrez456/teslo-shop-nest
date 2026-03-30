4a3085fc9a221cbb45c42246d0f8e7792d2686d4
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-29 21:30:00 -0500

    Instalacion de websocket para su implementacion

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
