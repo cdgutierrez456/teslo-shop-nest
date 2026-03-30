4ba56f0c815779bddb6f82d6fb842c6b8f392df9
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-29 21:38:47 -0500

    Implementando funciones inciales (sin logica) de conexion

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,11 +1,19 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('Socket connected ', client.id);
+  }
 
+  handleDisconnect(client: Socket) {
+    console.log('Socket disconnected ', client.id);
+
+  }
 
 }
